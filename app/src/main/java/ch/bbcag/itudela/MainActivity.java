65fb3888e0408d0457f20448845a94cb40efb818
taabrde2 <delano.bruggmann@swisscom.com>
2018-05-30 14:19:45 +0200

    Implement YouTube Player

--- app/src/main/java/ch/bbcag/itudela/MainActivity.java
+++ app/src/main/java/ch/bbcag/itudela/MainActivity.java
@@ -1,13 +1,41 @@
 package ch.bbcag.itudela;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import ch.bbcag.itudela.helper.YouTubeConnector;
+import ch.bbcag.itudela.model.VideoItem;
+
+public class MainActivity extends Activity {
+
+    private static final String YouTube_API_URL = "http://www.wiewarm.ch/api/v1/bad.json/";
+
+    private EditText searchInput;
+    private ListView videosFound;
+
+    private Handler handler;
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
 
     private TextView mTextMessage;
 
@@ -36,9 +64,76 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        searchInput = (EditText)findViewById(R.id.search_input);
+        videosFound = (ListView)findViewById(R.id.videos_found);
+
+        handler = new Handler();
+        addClickListener();
+
+        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    searchOnYoutube(v.getText().toString());
+                    return false;
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private List<VideoItem> searchResults;
+
+    private void searchOnYoutube(final String keywords){
+        new Thread(){
+            public void run(){
+                YouTubeConnector yc = new YouTubeConnector(MainActivity.this);
+                searchResults = yc.search(keywords);
+                handler.post(new Runnable(){
+                    public void run(){
+                        updateVideosFound();
+                    }
+                });
+            }
+        }.start();
+    }
+
+    private void updateVideosFound(){
+        ArrayAdapter<VideoItem> adapter = new ArrayAdapter<VideoItem>(getApplicationContext(), R.layout.video_item, searchResults){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null){
+                    convertView = getLayoutInflater().inflate(R.layout.video_item, parent, false);
+                }
+                ImageView thumbnail = (ImageView)convertView.findViewById(R.id.video_thumbnail);
+                TextView title = (TextView)convertView.findViewById(R.id.video_title);
+                TextView description = (TextView)convertView.findViewById(R.id.video_description);
+
+                VideoItem searchResult = searchResults.get(position);
+
+                Picasso.with(getApplicationContext()).load(searchResult.getThumbnailURL()).into(thumbnail);
+                title.setText(searchResult.getTitle());
+                description.setText(searchResult.getDescription());
+                return convertView;
+            }
+        };
+
+        videosFound.setAdapter(adapter);
+    }
+
+    private void addClickListener(){
+        videosFound.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos,
+                                    long id) {
+                Intent intent = new Intent(getApplicationContext(), PlayingMusicActivity.class);
+                intent.putExtra("VIDEO_ID", searchResults.get(pos).getId());
+                startActivity(intent);
+            }
+
+        });
     }
 
 }
---
+++ app/src/main/java/ch/bbcag/itudela/helper/Config.java
@@ -0,0 +1,10 @@
+package ch.bbcag.itudela.helper;
+
+public class Config {
+
+    private Config(){
+
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyBD53et9skt4WMNykFkdSh9J1Ek7eBw9hE";
+}
